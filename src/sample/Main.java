088eb7ce61cd709825ec11744351a3758640d933
Matthew Hoang <hoangmatthew1999@gmail.com>
2020-11-02 10:09:17 -0800

    got the front page to work properly

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,23 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.FlowPane;
+
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception{
-       window.setTitle("Hello");
-       StackPane root  = new StackPane();
+        window.setTitle("HBox Experiment 1");
 
-       Button button = new Button("Click me");
-       root.getChildren().add(button);
+        Button button1 = new Button("Add or Remove ");
+        Button button2 = new Button("Search or View Events");
+        Button button3 = new Button("Reschedule");
 
-       Scene scene = new Scene(root,400,400);
-       window.setScene(scene);
-       window.show();
+        FlowPane flowpane = new FlowPane();
+
+        flowpane.getChildren().add(button1);
+        flowpane.getChildren().add(button2);
+        flowpane.getChildren().add(button3);
+
+        Scene scene = new Scene(flowpane, 200, 100);
+        window.setScene(scene);
+        window.show();
     }
 
 
